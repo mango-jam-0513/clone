3bf8bdb948b0e62becdaab991b4945af1102d256
mango-jam-0513 <mangojamstar0513@outlook.com>
2020-11-02 23:44:09 +0800

    update

--- src/index.js
+++ src/index.js
@@ -2,40 +2,34 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import './index.css';
 
-const firstBook = {
-  img : '/images/hero.jpg',
-  title : 'Final Fantasy',
-  author : 'John Sammie',
-};
-const secondBook = {
-  img :'https://imgd.aeplcdn.com/1056x594/cw/ec/36756/MG-Hector-Right-Front-Three-Quarter-166301.jpg?wm=0&q=85',
-  title : 'Our class is my family.',
-  author : 'Mangojam',
-};
+const books = [
+  {
+    img : '/images/hero.jpg',
+    title : 'Final Fantasy',
+    author : 'John Sammie',
+  },
+  {
+    img :'https://imgd.aeplcdn.com/1056x594/cw/ec/36756/MG-Hector-Right-Front-Three-Quarter-166301.jpg?wm=0&q=85',
+    title : 'Our class is my family.',
+    author : 'Mangojam',
+  }
+]
 
 function BookList() {
   return (
     <section className='booklist'>
-      <Book img={firstBook.img} title={firstBook.title} author={firstBook.author}>
-        <p>dfakldjflajldfjlajsdlfjalsdjflakjdslfjakljdsflkajsdflj</p>
-      </Book>
-
-      <Book
-        img={secondBook.img}
-        title={secondBook.title}
-        author={secondBook.author}
-      />
+      {books}
     </section>
-  )
+  );
 }
 
-const Book = ({img,title,author,children}) => {
+const Book = (props) => {
+  const {img,title,author} = props;
   return(
     <article className="book">
       <img src={img} alt='' />
       <h1>{title}</h1>
       <h4>{author}</h4>
-      {children}
     </article>
   )
 }
